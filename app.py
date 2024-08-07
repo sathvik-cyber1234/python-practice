d88be2b63cc9cd5c0b05d8ef44fffb3da630b96e
sathvik-cyber1234 <sathvikbaswaraju@gmail.com>
2024-08-07 18:34:10 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+print("Sathvik Baswaraju")
+print('o----')
+print(" ||||")
+print("*" * 10)
+
+price = 10
+rating = 4.9
+name = "Sathvik"
+is_published = False
+print(price)
+
+full_name = "John Smith"
+age=20
+new_patient = True
+
+name = input("What is your name? ")
+print("Hi " + name)
+favorite_color = input("What is your favorite color? ")
+print("You like " + favorite_color )
+
+birth_year = input("Birth Year: ")
+age = 2024 - int(birth_year)
+print(age)
